d9b08d289d4345bcf0f900aa65c6969e6474cfce
JLake310 <jaeyeon.kim@nota.ai>
2024-12-19 12:18:42 +0000

    add scripts

---
+++ scripts/train.sh
@@ -0,0 +1,6 @@
+export ACCELERATE_USE_FSDP=1 
+export FSDP_CPU_RAM_EFFICIENT_LOADING=1 
+export NCCL_IB_DISABLE=1 
+
+
+CUDA_VISIBLE_DEVICES=6,7 accelerate launch train.py --cfg-path configs/train_stage1.yaml
